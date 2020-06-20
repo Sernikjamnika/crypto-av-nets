131bdd4002f3ba46c7983396ee6b68b2defacd85
Adrian Mucha <kestarumper8@gmail.com>
2020-06-20 10:41:38 +0200

    Added group generation.

--- controllers/Group.js
+++ controllers/Group.js
@@ -1,43 +1,71 @@
-const uniqueId = require('lodash/uniqueId');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const uniqueId = require("lodash/uniqueId");
+const uniq = require("lodash/uniq");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+const { gcd, isPrime, randomInt } = require("mathjs");
 
 async function generatePrime(bits) {
-    const { stdout, stderr } = await exec(`openssl prime -generate -bits ${bits}`);
-    if (stderr) {
-        throw new Error(stderr);
-    }
-    console.log({stdout})
-    return parseInt(stdout)
+  const { stdout, stderr } = await exec(
+    `openssl prime -generate -bits ${bits}`
+  );
+  if (stderr) {
+    throw new Error(stderr);
+  }
+  return parseInt(stdout);
 }
 
-function findR(p, q) {
-    for(let r = 1; ; r++) {
-        if (p == q * r + 1) {
-            return r;
-        }
+async function findPrimePair(r) {
+  for (let i = 0; i < 10000; i++) {
+    const q = await generatePrime(8);
+    const p = r * q + 1;
+    if (isPrime(p)) {
+      return { p, q };
     }
+  }
+  throw new Error("Cannot construct such a prime pair.");
 }
 
-function findGenerator(p, r) {
-    for(let h = 2; h < p; h++) {
-        const generator = pow(h, r) % p;
-        if (generator !== 1) {
-            return generator;
-        }
+function newCyclicMultiplicativeGroup(p) {
+  const Zp = [];
+  for (let a = 1; a < p; a++) {
+    if (gcd(a, p) === 1) {
+      Zp.push(a);
     }
-    throw new Error("No such `h` exists.")
+  }
+  return Zp;
+}
+
+function rthResiduesSubgroup(r, p) {
+  const Zp = newCyclicMultiplicativeGroup(p);
+  return uniq(Zp.map((h) => Math.pow(h, r) % p));
+}
+
+async function createGroup() {
+  const r = 2;
+  const { p, q } = await findPrimePair(r);
+  const G = rthResiduesSubgroup(r, p);
+  if(G.length !== q) {
+    throw new Error(`Bad group order: ${G.length} !== ${q}.`);
+  }
+  return { p, q, r, G };
+}
+
+function randomGenerator(G) {
+  const order = G.length;
+  while (true) {
+    const elementIndex = randomInt(0, order);
+    const g = G[elementIndex];
+    if (g !== 1) {
+      return g;
+    }
+  }
 }
 
 async function newGroup(question, n) {
-    const id = uniqueId();
-    const p = await generatePrime(64);
-    const q = await generatePrime(16);
-    // const r = findR(p, q);
-    // const g = findGenerator(p, r);
-    const g = 0;
-
-    return {n, question, id, p, q, g}
+  const id = uniqueId();
+  const { G, q } = await createGroup();
+  const g = randomGenerator(G);
+  return { n, question, id, q, G, g };
 }
 
 module.exports = newGroup;
--- routes/group.js
+++ routes/group.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const newGroup = require('../controllers/Group');
-
+const newGroup = require("../controllers/Group");
 
 /* GET home page. */
 router.post("/", async (req, res, next) => {
-  console.log(req.body)
-  const {body} = req;
-  const {question, n} = body;
-
-  const group = await newGroup(question, n);
-  
-  res.json(group)
+  try {
+    const { body } = req;
+    const { question, n } = body;
+    const group = await newGroup(question, n);
+    res.json(group);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send(error.message);
+  }
 });
 
 module.exports = router;
